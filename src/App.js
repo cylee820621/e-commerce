c0c9b882ffdb2bc07a1d3b69206c67e7a6f7a3d2
CYLee <cylee820621@gmail.com>
2022-01-01 15:11:07 -0500

    add header and header styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
+import Header from "./components/header/header.component";
 const HatsPage = () => {
   return (
     <div>
@@ -16,6 +16,7 @@ const HatsPage = () => {
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />}></Route>
         <Route path="/shop" element={<ShopPage />}></Route>
