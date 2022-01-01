aeec4bc4b313c021ed4fc5a727113e15ce768590
CYLee <cylee820621@gmail.com>
2022-01-01 15:00:59 -0500

    updating tith shop page, collection preview. collection item

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 const HatsPage = () => {
   return (
@@ -17,7 +18,7 @@ function App() {
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />}></Route>
-        <Route path="/hats" element={<HatsPage />}></Route>
+        <Route path="/shop" element={<ShopPage />}></Route>
       </Routes>
     </div>
   );
