1d8fe2f6d65c890efdf2496e4f391a5b9bcb8503
CYLee <cylee820621@gmail.com>
2022-01-02 06:18:37 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -15,24 +15,32 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
-    const { value, name } = event;
+    const { value, name } = event.target;
+
     this.setState({ [name]: value });
   };
 
   render() {
     return (
       <div className="sign-in">
-        <h2 className="title">I already have an account</h2>
+        <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput label="email" handleChange={this.handleChange} type="email" name="email" value={this.state.email} required />
-          <FormInput label="password" handleChange={this.handleChange} type="password" name="password" value={this.state.password} required />
+          <FormInput name="email" type="email" handleChange={this.handleChange} value={this.state.email} label="email" required />
+          <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
