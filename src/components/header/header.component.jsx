50f195923f195397004aab2696eb7965e2b514a0
CYLee <cylee820621@gmail.com>
2022-01-02 09:06:58 -0500

    add  carticon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { auth } from "../../firebase/firebase.utils";
+import { auth } from '../../firebase/firebase.utils';
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
